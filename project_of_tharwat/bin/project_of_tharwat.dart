257f2be40d755650ae580509b730ed1b48f33a89
Asmaa Alndhari <asmaaalnadhari@gmail.com>
2023-11-19 07:32:16 +0300

    add the structure

---
+++ project_of_tharwat/bin/project_of_tharwat.dart
@@ -0,0 +1,5 @@
+import 'package:project_of_tharwat/project_of_tharwat.dart' as project_of_tharwat;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_of_tharwat.calculate()}!');
+}
---
+++ project_of_tharwat/test/project_of_tharwat_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_of_tharwat/project_of_tharwat.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
